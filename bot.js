2863fa78e232d1ca470fc7380c07b5e0abdfd3b3
Alex <evseev.alex567@gmail.com>
2024-11-10 23:11:40 +0300

    Восстановлен файл bot.js

---
+++ bot.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const token = process.env.BOT_TOKEN;
+const TelegramBot = require('node-telegram-bot-api');
+const bot = new TelegramBot(token, { polling: true });
+
+const webAppUrl = 'https://sensational-hotteok-80249a.netlify.app/';
+
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Откройте мини-приложение:', {
+    reply_markup: {
+      inline_keyboard: [
+        [{ text: 'Открыть приложение', web_app: { url: webAppUrl } }]
+      ]
+    }
+  });
+});
